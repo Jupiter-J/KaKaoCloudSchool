9d50c5c8fa6fca809d57f2dfe1adf2c99822bb51
Jupiter-J <namju2912@gmail.com>
2022-07-27 17:35:19 +0900

    basic project

---
+++ servlet/ReStudy/ReProjectMaven 복사본/src/main/java/book/controller/BookSearchController.java
@@ -0,0 +1,8 @@
+package book.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet
+public class BookSearchController extends HttpServlet {
+}
